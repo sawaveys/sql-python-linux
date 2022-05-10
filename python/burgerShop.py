1b2c21a718171f8a46cb85f17d41106246a28e73
Safaa <abukarsafa@gmail.com>
2022-05-10 21:02:37 +0100

    burger

--- python/burgerShop.py
+++ python/burgerShop.py
@@ -1,3 +1,10 @@
 #safaa abukar 
 #10/5/2022
 
+class FoodItem: 
+    #constructor 
+    def_init_(self, name, itemType, price):
+        #typically common for all classes
+        self.itemName= name
+        self.foodType = itemType
+        self.price = float(price)
